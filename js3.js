c9b7932d9783f9841ec5aa0e5f27fc7255b5ddd1
Prakash4567 <67901883+Prakash4567@users.noreply.github.com>
2020-12-11 09:23:53 +0530

    Add files via upload

---
+++ js3.js
@@ -0,0 +1,23 @@
+ //Called Functions(Parameter List--> PL; Return value--> RLf)
+            // 1) with PL & RL
+            function sum1(a,b)
+            {
+                return (a+b);
+            }
+            // 2) without PL & RL
+            function sum2()
+            {
+                var a=2,b=22
+                return (a+b)
+            }
+            // 3) with PL & withot RL
+            function sum3(a,b)
+            {
+                document.write((a+b)+ '<br>')
+            }
+            // 4) without PL & with RL
+            function sum4()
+            {
+                var a=4,b=44
+                document.write((a+b)+ '<br>')
+            }
